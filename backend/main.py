6cded0cc3abe9734c44ee558b4f9c212af35cb8b
SharathKumar444 <ksharathkumar2021@gmail.com>
2026-05-17 09:58:28 +0530

    first commit

---
+++ backend/main.py
@@ -0,0 +1,85 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# CORS (React connection)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Mock database
+users = [
+    {
+        "id": 1,
+        "name": "Sharath",
+        "email": "sharath@example.com",
+        "role": "Frontend Developer",
+        "bio": "Passionate React developer",
+        "company": "Tech Solutions",
+        "website": "https://sharath.dev"
+    },
+    {
+        "id": 2,
+        "name": "Rahul",
+        "email": "rahul@example.com",
+        "role": "Backend Developer",
+        "bio": "Python and API developer",
+        "company": "CodeCraft",
+        "website": "https://rahul.dev"
+    }
+]
+
+# Validation model
+class User(BaseModel):
+    name: str
+    email: str
+    role: str
+    bio: str
+    company: str
+    website: str
+
+
+@app.get("/")
+def home():
+    return {"message": "Backend Running Successfully"}
+
+
+@app.get("/users")
+def get_users():
+    return {"success": True, "data": users}
+
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    for user in users:
+        if user["id"] == user_id:
+            return {"success": True, "data": user}
+
+    raise HTTPException(status_code=404, detail="User not found")
+
+
+@app.post("/users")
+def create_user(user: User):
+    new_user = {
+        "id": len(users) + 1,
+        "name": user.name,
+        "email": user.email,
+        "role": user.role,
+        "bio": user.bio,
+        "company": user.company,
+        "website": user.website
+    }
+
+    users.append(new_user)
+
+    return {
+        "success": True,
+        "message": "User created successfully",
+        "data": new_user
+    }
---
+++ backend/users.py
@@ -0,0 +1,20 @@
+users = [
+    {
+        "id": 1,
+        "name": "Sharath",
+        "email": "sharath@example.com",
+        "role": "Frontend Developer",
+        "bio": "Passionate about React and UI development.",
+        "company": "Tech Solutions",
+        "website": "https://sharath.dev"
+    },
+    {
+        "id": 2,
+        "name": "Rahul",
+        "email": "rahul@example.com",
+        "role": "Backend Developer",
+        "bio": "Loves building APIs using Python.",
+        "company": "CodeCraft",
+        "website": "https://rahul.dev"
+    }
+]
